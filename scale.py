903904bb057dacf65ebad7b4e643d25d3d46a000
hahahellooo <hahahello777@gmail.com>
2024-10-30 10:43:54 +0900

    test done

--- scale.py
+++ scale.py
@@ -9,30 +9,29 @@ def check_CPU(n):
         r = subprocess.check_output(["docker", "stats", f"me-blog-{n}", "--no-stream", "--format", "{{json .}}"])
         j = json.loads(r.decode("utf-8"))
         CPU.append(float(j['CPUPerc'].strip('%')))
-    print(sum(CPU))
     return sum(CPU)
 
 
 while True:
+
     # 실행 중인 도커 개수 카운트
     r = subprocess.check_output(["docker", "ps", "--filter","name=me-blog", "--format", "{{.Names}}"])
     blog = r.decode("utf-8").strip().split("\n")
     blogcount=len(blog)
-    print(len(blog))
-    
-    useCPU=check_CPU(blogcount) 
-    # 전체 CPU 사용량이 50%를 넘으면 
-    # n+1개로 scale out
-    if useCPU > 0.5:
-        print(f"CPU 사용량이 50%를 넘었습니다.{blogcount+1}개로 scale out을 진행합니다.")
-        os.system(f"docker compose -f /home/hahahellooo/code/docker/kube1s/docker-compose.yml up -d --scale blog={blogcount+1}")
+    useCPU=round(check_CPU(blogcount),3) 
 
-    # 전체 CPU 사용량이 20% 미만이면
-    # 1개로 scale in
-    elif useCPU < 0.2:
-        if blogcount >1:
-            print(f"현재 blog는 {blogcount}개로, CPU 사용량이 20% 미만입니다. 1개로 scale in을 진행합니다.")
-            os.system(f"docker compose -f /home/hahahellooo/code/docker/kube1s/docker-compose.yml up -d --scale blog=1")
-    os.system("sleep 5")
+    # 전체 CPU 사용량이 50%를 넘으면 
+    # n-1개로 scale in
+    if useCPU > 0.5:
+        if blogcount>1:
+            print(f"현재 blog는 {blogcount}개, CPU 사용량은 {useCPU}로 50%를 넘었습니다.{blogcount-1}개로 scale in을 진행합니다.")
+            os.system(f"docker compose -f /home/hahahellooo/code/docker/kube1s/docker-compose.yml up -d --scale blog={blogcount-1}")
+
+    # 전체 CPU 사용량이 10% 미만이면
+    # n+1개로 scale out
+    elif useCPU < 0.1:
+        print(f"현재 blog는 {blogcount}개, CPU 사용량은 {useCPU}로 10% 미만입니다. {blogcount+1}개로 scale out을 진행합니다.")
+        os.system(f"docker compose -f /home/hahahellooo/code/docker/kube1s/docker-compose.yml up -d --scale blog={blogcount+1}")
+os.system("sleep 5")
 
 
