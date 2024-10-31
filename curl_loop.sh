e653922463b3166969862713c99e720e40c73701
hahahellooo <hahahello777@gmail.com>
2024-10-31 10:20:11 +0900

    local done

--- curl_loop.sh
+++ curl_loop.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# 등호 앞뒤로 공백이 없어야 함
-
 # 기본값 설정
 n=${1:-5}
 nn=${2:-1}
@@ -9,9 +7,9 @@ nn=${2:-1}
 # 요청 URL 설정
 url="http://localhost:8949"
 
-for ((i=1;, i<=n;, i++)); do
-        echo "Request #$i"
-        curl -I "$url"
-        echo ""
-        sleep "$nn"
+for ((i=1; i<=n; i++)); do
+  echo "Request #$i"
+  curl -s "$url"
+  echo ""
+  sleep "$nn"
 done
