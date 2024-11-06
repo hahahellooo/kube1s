0464b4ec9f06dcdac98762efcffcdc2d6f09ae23
hahahellooo <hahahello777@gmail.com>
2024-11-06 15:46:32 +0900

    add logfile

--- src/kube1s/scale.py
+++ src/kube1s/scale.py
@@ -8,6 +8,8 @@ import requests
 api_url = 'https://notify-api.line.me/api/notify'
 key = os.getenv('LINE_TOKEN')
 
+start_time = 0
+
 # 사용중인 CPU 크기 확인
 def check_CPU(n):
     CPU=[]
@@ -17,8 +19,22 @@ def check_CPU(n):
         CPU.append(float(j['CPUPerc'].strip('%')))
     return sum(CPU)
 
-def auto_controll():
+# 로그파일 만들기
+def save_log(event):
+    file_name='scale_log.txt'
+    my_path = __file__
+    dir_name = os.path.dirname(my_path)
 
+    # 디렉토리가 없을 경우 생성
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name, exist_ok = True)
+    file_path = os.path.join(dir_name, file_name)
+    
+    with open(file_path, 'a') as f:
+        f.write(f"{time.strftime('%Y-%m-%s %H:%M:%S')} / {event}\n")
+
+# auto scale in & out
+def auto_controll():
     
     while True:
 
@@ -26,33 +42,45 @@ def auto_controll():
         r = subprocess.check_output(["docker", "ps", "--filter","name=me-blog", "--format", "{{.Names}}"])
         blog = r.decode("utf-8").strip().split("\n")
         blogcount=len(blog)
-        useCPU=round(check_CPU(blogcount),4)
-        max_time= time.time() + (60)
+        useCPU=check_CPU(blogcount)
 
-    # 전체 CPU 사용량이 10%를 넘고 1분 이상 지속되면
+    # 전체 CPU 사용량이 50%를 넘고 1분 이상 지속되면
     # n+1개로 scale out
-        if useCPU > 0.1 and time.time() >= max_time:
-            print(f"현재 blog는 {blogcount}개, CPU 사용량은 {useCPU}로 10%를 넘었습니다.1분 이상 지속되어 {blogcount+1}개로 scale out을 진행합니다.")
-            os.system(f"docker compose -f /home/hahahellooo/code/docker/kube1s/docker-compose.yml up -d --scale blog={blogcount+1}")
-            request = requests.post(url=api_url, headers =  {'Authorization':'Bearer ' + key}, data = {'message' : f'CUP사용량이 10%를 넘어 {blogcount+1}개로 scale out 진행 중'})
-            max_time = time.time() + 60  # scale out 후 1분 타이머 초기화
-    # 전체 CPU 사용량이 10%를 넘지만 1분 미만 지속되면
-    # n-1개로 scale in
-        elif useCPU> 0.1 and time.time() < max_time:
-            if blogcount>1:
-                print(f"현재 blog는 {blogcount}개, CPU 사용량은 {useCPU}로 10%를 넘었습니다. 1분 이상 지속되지 않아 {blogcount-1}개로 scale in을 진행합니다.")
-                os.system(f"docker compose -f /home/hahahellooo/code/docker/kube1s/docker-compose.yml up -d --scale blog={blogcount-1}")
-                request = requests.post(url=api_url, headers =  {'Authorization':'Bearer ' + key}, data = {'message' : f'CPU사용량이 10%를 넘었지만 1분 이상 지속되지 않아 {blogcount-1}개로 scale in 진행 중'})
-                max_time = time.time() + 60  # scale in 후 1분 타이머 초기화
+        if useCPU > 0.5:
+            if not start_time:
+                start_time = time.time()
+            else: 
+                end_time = time.time()
+                if end_time - start_time >=60.00:
+                    print(f"현재 blog는 {blogcount}개, CPU 사용량은 {useCPU}로 50%를 넘었습니다. 1분 이상 지속되어 {blogcount+1}개로 scale out을 진행합니다.")
+                    os.system(f"docker compose -f /home/hahahellooo/code/docker/kube1s/docker-compose.yml up -d --scale blog={blogcount+1}")
+                    save_log(f"scale out / {blogcount+1}")
+                    request = requests.post(url=api_url, headers =  {'Authorization':'Bearer ' + key}, data = {'message' : f'{blogcount+1}개로 scale out 진행 중'})
+                    start_time = 0 # 조건문 진행되면 시간 초기화 
 
-    # 전체 CPU 사용량이 10% 미만이면
+    # 전체 CPU 사용량이 50%를 넘지만 1분 미만 지속되면
+    # n-1개로 scale in
+        elif useCPU> 0.5 and blogcount>1:
+            if not start_time:
+                start_time=time.time()
+            else:
+                end_time= time.time()
+                if end_time - start_time >= 60.00:                
+                    print(f"현재 blog는 {blogcount}개, CPU 사용량은 {useCPU}로 50%를 넘었습니다. 1분 이상 지속되지 않아 {blogcount-1}개로 scale in을 진행합니다.")
+                    os.system(f"docker compose -f /home/hahahellooo/code/docker/kube1s/docker-compose.yml up -d --scale blog={blogcount-1}")
+                    save_log(f"scale in  / {blogcount-1}")
+                    request = requests.post(url=api_url, headers =  {'Authorization':'Bearer ' + key}, data = {'message' : f'1분 이상 지속되지 않아 {blogcount-1}개로 scale in 진행 중'})
+                    start_time = 0
+
+    # 전체 CPU 사용량이 2% 미만이면
     # 1개로 scale in
-        elif useCPU < 0.02 and time.time() >= max_time:
+        elif useCPU < 0.02:
             print(f"현재 blog는 {blogcount}개, CPU 사용량은 {useCPU}로 2% 미만입니다. 1개로 scale in을 진행합니다.")
             os.system(f"docker compose -f /home/hahahellooo/code/docker/kube1s/docker-compose.yml up -d --scale blog=1")
+            save_log(f"scale in  / 1")
             request = requests.post(url=api_url, headers =  {'Authorization':'Bearer ' + key}, data = {'message' : f'CPU 사용량 2% 미만, 1개로 scale in 진행 중'})
-            max_time = time.time() + 60  # scale in 후 1분 타이머 초기화
-    
-    # 10초 마다 사용량 체크 
-    time.sleep(10)
+            start_time = 0
+
+        # 10 초마다 동작
+        time.sleep(10)
 
